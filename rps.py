c1307ece17df72b9c54b6e25302ef1e233bd71d1
tooltime <tooltime@mohawkcollege.ca>
2024-11-25 13:10:04 -0500

    feature implemented. Fixes #3

--- rps.py
+++ rps.py
@@ -4,25 +4,36 @@
 
 import random
 
-user_action = input("Enter throw (rock, paper, scissors): ")
-ai_action = random.choice(["rock", "paper", "scissors"])
+while True:
+    # Get user's input
+    user_action = input("Enter throw (rock, paper, scissors): ").lower()
+    ai_action = random.choice(["rock", "paper", "scissors"])
 
-print(f"\nYou chose {user_action}, AI chose {ai_action}.\n")
+    print(f"\nYou chose {user_action}, AI chose {ai_action}.\n")
 
-if user_action == ai_action:
-    print(f"Both players selected {user_action}. It's a tie!")
-elif user_action == "rock":
-    if ai_action == "scissors":
-        print("Rock smashes scissors! You win!")
+    if user_action == ai_action:
+        print(f"Both players selected {user_action}. It's a tie!")
+    elif user_action == "rock":
+        if ai_action == "scissors":
+            print("Rock smashes scissors! You win!")
+        else:
+            print("Paper covers rock! You lose.")
+    elif user_action == "paper":
+        if ai_action == "rock":
+            print("Paper covers rock! You win!")
+        else:
+            print("Scissors cuts paper! You lose.")
+    elif user_action == "scissors":
+        if ai_action == "paper":
+            print("Scissors cuts paper! You win!")
+        else:
+            print("Rock smashes scissors! You lose.")
     else:
-        print("Paper covers rock! You lose.")
-elif user_action == "paper":
-    if ai_action == "rock":
-        print("Paper covers rock! You win!")
-    else:
-        print("Scissors cuts paper! You lose.")
-elif user_action == "scissors":
-    if ai_action == "paper":
-        print("Scissors cuts paper! You win!")
-    else:
-        print("Rock smashes scissors! You lose.")
+        print("Invalid input. Please enter rock, paper, or scissors.")
+
+    play_again = input("Do you want to play again? (yes/no): ").lower()
+    if play_again != "yes":
+        print("Thanks for playing! Goodbye!")
+        break
+
+
